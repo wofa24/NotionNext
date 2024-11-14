c56a2824f78df0c2eb0e3ae6faef1dfad9bca4b0
wofa24 <haow6985@gmail.com>
2024-11-14 14:16:30 +0800

    Update config.js

--- themes/example/config.js
+++ themes/example/config.js
@@ -13,7 +13,7 @@ const CONFIG = {
   EXAMPLE_TITLE_IMAGE: false, // 标题栏，是否背景图片
 
   // 文章页面布局
-  EXAMPLE_ARTICLE_LAYOUT_VERTICAL: false, // 文章详情，左右布局改为上下布局
-  EXAMPLE_ARTICLE_HIDDEN_NOTIFICATION: false // 文章详情隐藏公告
+  EXAMPLE_ARTICLE_LAYOUT_VERTICAL: ture, // 文章详情，左右布局改为上下布局
+  EXAMPLE_ARTICLE_HIDDEN_NOTIFICATION: true // 文章详情隐藏公告
 }
 export default CONFIG
